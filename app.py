c46babd0d765d333e3bdaeb0e6ecdb639818373b
justakris <k.s.bonev@gmail.com>
2024-06-09 22:50:14 +0300

    Forgot the models file

--- app.py
+++ app.py
@@ -1,21 +1,22 @@
 # streamlit_app/app.py
 
 import streamlit as st
-from src.utils.utils import preprocess_image, load_model, predict
+from src.utils.utils import get_best_model_path, load_model, predict, preprocess_image
 from src.config import CLASSES
 
 # Set up the Streamlit interface
 st.title("Chest X-Ray Image Classification")
 
 # File uploader
-uploaded_file = st.file_uploader("Choose a chest X-ray image...", type="jpg")
+uploaded_file = st.file_uploader("Choose a chest X-ray image...", type=["jpg", "png"])
 
 if uploaded_file is not None:
     # Preprocess the image
     image = preprocess_image(uploaded_file)
     
     # Load the model
-    model = load_model('./notebooks/Checkpoints/MobileNet_transfer/MobileNet_transfer-0.73-09-408449089731872812-b8b19d47e4f446889dd4418ea0ba3644.keras')
+    # model = load_model('./notebooks/Checkpoints/MobileNet_transfer/MobileNet_transfer-0.73-09-408449089731872812-b8b19d47e4f446889dd4418ea0ba3644.keras')
+    model = load_model(get_best_model_path())
     
     # Make prediction
     prediction = CLASSES[int(predict(model, image))]
---
+++ src/models/models.py
@@ -0,0 +1,55 @@
+from tensorflow.keras.applications import EfficientNetB0, MobileNetV3Small # type: ignore
+from tensorflow.keras.models import Model # type: ignore
+from tensorflow.keras.layers import BatchNormalization, Dense, Dropout, Flatten, GlobalAveragePooling2D # type: ignore
+from src.config import INPUT_SHAPE, NUM_CLASSES
+
+
+def MobileNetv3Transfer():
+    base_model = MobileNetV3Small(weights='imagenet', include_top=False, input_shape=INPUT_SHAPE)
+
+    base_model.trainable = False  # Freeze the base model initially
+
+    x = GlobalAveragePooling2D()(base_model.output)
+    x = Flatten()(x)
+    x = Dense(512, activation='relu')(x)
+    x = Dropout(0.5)(x)
+    x = Dense(256, activation='relu')(x)
+    x = Dropout(0.4)(x)
+    x = Dense(128, activation='relu')(x)
+    x = Dropout(0.3)(x)
+    x = Dense(64, activation='relu')(x)
+    x = Dropout(0.2)(x)
+    outputs = Dense(4, activation="softmax")(x)
+
+    model = Model(inputs=base_model.input, outputs=outputs)
+    return model
+
+
+def EfficientNetTransfer():
+    base_model = EfficientNetB0(weights='imagenet', include_top=False, input_shape=INPUT_SHAPE)
+    
+    base_model.trainable = False  # Freeze the base model initially
+
+    x = GlobalAveragePooling2D()(base_model.output)
+    x = BatchNormalization()(x)
+
+    x = Dense(512, activation='relu')(x)
+    x = Dropout(0.5)(x)
+    x = BatchNormalization()(x)
+
+    x = Dense(256, activation='relu')(x)
+    x = Dropout(0.4)(x)
+    x = BatchNormalization()(x)
+
+    x = Dense(128, activation='relu')(x)
+    x = Dropout(0.3)(x)
+    x = BatchNormalization()(x)
+
+    x = Dense(64, activation='relu')(x)
+    x = Dropout(0.2)(x)
+    x = BatchNormalization()(x)
+
+    output = Dense(NUM_CLASSES, activation='softmax')(x)
+
+    model = Model(inputs=base_model.input, outputs=output)
+    return model
